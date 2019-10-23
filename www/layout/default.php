bc827a27db85016e533a9e82753d3935b084260a
Bismarck6502 <sam@aepearce.com>
2019-10-23 21:35:42 +0200

    Set up framework

---
+++ www/layout/default.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<title>PolSim | <?= $page_title ?></title>
+		
+	</head>
+	<body>
+
+		<!-- Main Page Content -->
+		<h1><?= $page_title ?></h1>
+		<hr>
+		<?php include $page_content ?>
+
+	</body>
+</html>
